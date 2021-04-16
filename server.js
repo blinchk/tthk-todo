edbf9bdb4319e1fb80d91ff6a946fe221a193252
Nikolas Laus <bchik3@gmail.com>
2021-04-16 11:31:41 +0300

    Add date generation and TO-DO List.

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const date = require("./generateDate");
 const app = express();
 
+let todoList = [];
+
+app.set("view engine", ejs);
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    let dateToday = date.getDate();
+
+    res.render("index.ejs", {
+        dateFromServer: dateToday,
+        todoItems: todoList
+    });
+});
+
+app.post("/", (req, res) => {
+    const todoItem = req.body.newTask;
+    todoList.push(todoItem);
+    res.redirect("/");
+});
+
 app.listen(3000, () => {
-    console.log(`Server is running on 3000`);
-})
+  console.log(`Server is running on 3000`);
+});
